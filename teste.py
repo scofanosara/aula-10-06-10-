537530b6197786a55ac30c73b879a01106be65e9
scofanosara <scofanosara@gmail.com>
2025-10-12 16:42:55 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,8 +1,15 @@
-import streamlit as st 
+import streamlit as st
 
 st.title("Meu programa")
-st.write("Alô mundo")
 
-nome = st.text_input("Digite seu nome: ")
+st.write("Gerador de nome completo")
+
+nome = st.text_input("Digite o seu primeiro nome:")
 if nome:
   st.write(nome.upper())
+sobrenome = st.text_input("Digite o seu sobrenome:")
+if sobrenome:
+  st.write(nome.upper())
+
+if st.button("Mostrar nome completo"):
+    st.success(f"O seu nome completo é {nome} {sobrenome}")

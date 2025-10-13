ce151a457f8da08ad6685fd43ef0e35f790ad136
scofanosara <scofanosara@gmail.com>
2025-10-13 15:05:30 -0300

    Create paises(2).py

---
+++ paises(2).py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import streamlit as st
+import plotly.express as px
+
+dataset = pd.read_csv('https://www.irdx.com.br/media/uploads/paises.csv')
+
+fig = px.choropleth(dataset,
+                     locations=dataset['iso3'],
+                     color=dataset['longitude'],
+                     hover_name=dataset['nome'])
+fig.update_layout(title='Mapa Coroplético dos Países',
+                  geo_scope='world')
+fig.show()
+
